20f3a52743708040ac1355b82ca2fa67ada4a0d2
belljun3395 <195850@jnu.ac.kr>
2025-02-12 16:37:57 +0900

    feat: 초기 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.2"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("com.vaadin") version "24.6.5"
+    kotlin("jvm") version DependencyVersion.KOTLIN
+    kotlin("plugin.spring") version DependencyVersion.KOTLIN
+    kotlin("plugin.allopen") version DependencyVersion.KOTLIN
+    kotlin("kapt") version DependencyVersion.KOTLIN
+
+    /** spring */
+    id("org.springframework.boot") version DependencyVersion.SPRING_BOOT
+    id("io.spring.dependency-management") version DependencyVersion.SPRING_DEPENDENCY_MANAGEMENT
+
+    /** docs */
+    id("org.springdoc.openapi-gradle-plugin") version DependencyVersion.SPRING_OPENAPI
+
+    /** lint */
+    id("org.jlleitschuh.gradle.ktlint") version DependencyVersion.KTLINT
 }
 
 group = "com.manage"
@@ -19,40 +28,96 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2024.0.0"
-extra["springModulithVersion"] = "1.3.1"
-extra["vaadinVersion"] = "24.6.5"
+/**
+ * https://kotlinlang.org/docs/reference/compiler-plugins.html#spring-support
+ * automatically supported annotation
+ * @Component, @Async, @Transactional, @Cacheable, @SpringBootTest,
+ * @Configuration, @Controller, @RestController, @Service, @Repository.
+ * jpa meta-annotations not automatically opened through the default settings of the plugin.spring
+ */
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("org.springframework.data.relational.core.mapping.Table")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${DependencyVersion.SPRING_MODULITH}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${DependencyVersion.SPRING_CLOUD}")
+    }
+}
 
 dependencies {
+    /** kotlin */
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    /** spring */
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.vaadin:vaadin-spring-boot-starter")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
     implementation("org.springframework.modulith:spring-modulith-starter-core")
-    runtimeOnly("com.mysql:mysql-connector-j")
-    runtimeOnly("io.asyncer:r2dbc-mysql")
     runtimeOnly("org.springframework.modulith:spring-modulith-actuator")
     runtimeOnly("org.springframework.modulith:spring-modulith-observability")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:${DependencyVersion.SPRINGDOC}")
+
+    /** data */
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("io.asyncer:r2dbc-mysql")
+    testRuntimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("com.zaxxer:HikariCP:${DependencyVersion.HIKARI_CP}")
+
+    /** flyway */
+    implementation("org.flywaydb:flyway-core:${DependencyVersion.FLYWAY}")
+    implementation("org.flywaydb:flyway-mysql")
+
+    /** domain */
+    implementation("org.jmolecules.integrations:jmolecules-starter-ddd:${DependencyVersion.JMOLECULES}")
+
+    /** aspectj */
+    implementation("org.aspectj:aspectjweaver:${DependencyVersion.ASPECTJ}")
+
+    /** aws */
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs:${DependencyVersion.AWS_SQS}")
+    implementation("software.amazon.awssdk:scheduler:${DependencyVersion.SCHEDULER}")
+    implementation("com.amazonaws:aws-java-sdk-ses:${DependencyVersion.AWS_SES}")
+
+    /** docs */
+    runtimeOnly("com.github.therapi:therapi-runtime-javadoc-scribe:${DependencyVersion.JAVADOC_SCRIBE}")
+    kapt("com.github.therapi:therapi-runtime-javadoc-scribe:${DependencyVersion.JAVADOC_SCRIBE}")
+
+    /** jsoup */
+    implementation("org.jsoup:jsoup:${DependencyVersion.JSOUP}")
+
+    /** test */
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
     testImplementation("org.springframework.modulith:spring-modulith-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    testImplementation("io.mockk:mockk:${DependencyVersion.MOCKK}")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:${DependencyVersion.MOCKITO_KOTLIN}")
+    testImplementation("io.kotest:kotest-runner-junit5:${DependencyVersion.KOTEST}")
+    testImplementation("io.kotest:kotest-assertions-core:${DependencyVersion.KOTEST}")
+    testImplementation("io.kotest:kotest-framework-api:${DependencyVersion.KOTEST}")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:${DependencyVersion.KOTEST_EXTENSION}")
+    testImplementation("io.kotest.extensions:kotest-extensions-allure:${DependencyVersion.KOTEST_EXTENSION}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${DependencyVersion.COROUTINE_TEST}")
 
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
-        mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-    }
+    /** logger */
+    implementation("io.github.oshai:kotlin-logging-jvm:${DependencyVersion.KOTLIN_LOGGING}")
 }
 
 kotlin {
@@ -64,3 +129,11 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+openApi {
+    customBootRun {
+        jvmArgs = listOf("-Dspring.profiles.active=local")
+    }
+}
+
+springBoot.buildInfo { properties { } }
---
+++ buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,54 @@
+object DependencyVersion {
+    /** kotlinVersion */
+    const val KOTLIN = "1.9.25"
+
+    /** ktlint */
+    const val KTLINT = "11.6.1"
+
+    /** springBootVersion */
+    const val SPRING_BOOT = "3.4.2"
+
+    /** springDependencyManagementVersion */
+    const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.7"
+
+    /** springModulith */
+    const val SPRING_MODULITH = "1.3.1"
+
+    /** springCloud */
+    const val SPRING_CLOUD = "2024.0.0"
+
+    /** springdoc */
+    const val SPRINGDOC = "2.3.0"
+    const val SPRING_OPENAPI = "1.9.0"
+    const val JAVADOC_SCRIBE = "0.15.0"
+
+    /** data */
+    const val HIKARI_CP = "6.2.1"
+
+    /** aws */
+    const val AWS_SQS = "3.0.1"
+    const val SCHEDULER = "2.29.45"
+    const val AWS_SES = "1.12.765"
+
+    /** jmolecules */
+    const val JMOLECULES = "0.24.1"
+
+    /** AspectJ **/
+    const val ASPECTJ = "1.9.5"
+
+    /** jsoup */
+    const val JSOUP = "1.15.3"
+
+    /** flyway */
+    const val FLYWAY = "10.12.0"
+
+    /** test */
+    const val MOCKITO_KOTLIN = "5.4.0"
+    const val MOCKK = "1.13.9"
+    const val KOTEST = "5.8.0"
+    const val KOTEST_EXTENSION = "1.3.0"
+    const val COROUTINE_TEST = "1.8.0"
+
+    /** Logger **/
+    const val KOTLIN_LOGGING = "7.0.0"
+}
