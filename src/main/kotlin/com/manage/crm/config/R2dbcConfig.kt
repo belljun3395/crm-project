5a2f717957f8ecb000a1e9b87ba72cfd9c763f5d
belljun3395 <195850@jnu.ac.kr>
2025-02-24 23:28:15 +0900

    refactor: repository 스캔 설정 모듈 별로 분리
    - 공통 설정시 모듈별로 테스트 불가능해짐

--- src/main/kotlin/com/manage/crm/config/R2dbcConfig.kt
+++ src/main/kotlin/com/manage/crm/config/R2dbcConfig.kt
@@ -15,14 +15,12 @@ import org.springframework.data.convert.CustomConversions
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
 import org.springframework.data.r2dbc.dialect.DialectResolver
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.r2dbc.connection.R2dbcTransactionManager
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.transaction.ReactiveTransactionManager
 
 @Configuration
 @EnableR2dbcAuditing
-@EnableR2dbcRepositories(basePackages = ["com.manage"])
 class R2dbcConfig {
     companion object {
         const val TRANSACTION_MANAGER = "transactionManager"
