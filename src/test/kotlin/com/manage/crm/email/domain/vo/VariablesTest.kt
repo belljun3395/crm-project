e829e60e337a2b3b82d1c801aa97ef136f5ba0bc
belljun3395 <195850@jnu.ac.kr>
2025-03-19 12:23:01 +0900

    test(email): Variables 테스트 추가

---
+++ src/test/kotlin/com/manage/crm/email/domain/vo/VariablesTest.kt
@@ -0,0 +1,65 @@
+package com.manage.crm.email.domain.vo
+
+import io.kotest.core.spec.style.FeatureSpec
+import io.kotest.matchers.shouldBe
+
+class VariablesTest : FeatureSpec({
+
+    feature(("Variables#isEmpty")) {
+        scenario("check if variables is empty") {
+            // given
+            val variables = Variables()
+
+            // then
+            variables.isEmpty() shouldBe true
+        }
+    }
+
+    feature("Variables#getVariables") {
+        scenario("get variables") {
+            // given
+            val variables = Variables("title:hello", "name")
+
+            // when
+            val result = variables.getVariables()
+
+            // then
+            result shouldBe listOf("title:hello", "name")
+        }
+
+        scenario("get variables without default") {
+            // given
+            val variables = Variables("title:hello", "name")
+
+            // when
+            val result = variables.getVariables(withDefault = false)
+
+            // then
+            result shouldBe listOf("title", "name")
+        }
+    }
+
+    feature("Variables#getVariable") {
+        scenario("get variable") {
+            // given
+            val variables = Variables("title:hello", "name")
+
+            // when
+            val result = variables.getVariable("title")
+
+            // then
+            result shouldBe "title:hello"
+        }
+
+        scenario("get variable without default") {
+            // given
+            val variables = Variables("title:hello", "name")
+
+            // when
+            val result = variables.getVariable("title", withDefault = false)
+
+            // then
+            result shouldBe "title"
+        }
+    }
+})
