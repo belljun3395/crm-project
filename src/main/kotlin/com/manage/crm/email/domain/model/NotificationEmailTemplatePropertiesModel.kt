1ebf99804d5e7e29219a29bcdb41d25c73fe9475
belljun3395 <195850@jnu.ac.kr>
2025-03-24 14:06:29 +0900

    feat(email): variables이 존재하지 않는지 확인하는 메서드 구현

--- src/main/kotlin/com/manage/crm/email/domain/model/NotificationEmailTemplatePropertiesModel.kt
+++ src/main/kotlin/com/manage/crm/email/domain/model/NotificationEmailTemplatePropertiesModel.kt
@@ -1,6 +1,13 @@
 package com.manage.crm.email.domain.model
 
+import com.manage.crm.email.domain.vo.Variables
+
 data class NotificationEmailTemplatePropertiesModel(
     val subject: String,
-    val body: String
-)
+    val body: String,
+    val variables: Variables
+) {
+    fun isNoVariables(): Boolean {
+        return variables.isEmpty()
+    }
+}
