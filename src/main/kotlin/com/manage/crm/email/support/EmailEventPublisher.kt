14c57d07aee3026d0e1e686e135d2fef3d4184f7
belljun3395 <195850@jnu.ac.kr>
2025-03-24 21:59:23 +0900

    feat(email): PostEmailTemplateEvent 발행 메서드 추가
    - arrow 사용한 구현: parMap
    - TransactionSynchronizationManager.forCurrentTransaction에 afterCompletion 추가 등록하여 applicationEventPublisher를 통한 이벤트 발행 로직 추가

--- src/main/kotlin/com/manage/crm/email/support/EmailEventPublisher.kt
+++ src/main/kotlin/com/manage/crm/email/support/EmailEventPublisher.kt
@@ -1,17 +1,26 @@
 package com.manage.crm.email.support
 
+import arrow.fx.coroutines.parMap
 import com.manage.crm.email.event.schedule.CancelScheduledEvent
 import com.manage.crm.email.event.send.EmailSendEvent
 import com.manage.crm.email.event.send.EmailSentEvent
 import com.manage.crm.email.event.send.notification.NotificationEmailSendTimeOutEvent
 import com.manage.crm.email.event.send.notification.NotificationEmailSendTimeOutInvokeEvent
+import com.manage.crm.email.event.template.PostEmailTemplateEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
+import org.springframework.transaction.reactive.TransactionSynchronization
+import org.springframework.transaction.reactive.TransactionSynchronizationManager
+import reactor.core.publisher.Mono
 
 @Component
 class EmailEventPublisher(
-    val applicationEventPublisher: ApplicationEventPublisher
+    private val applicationEventPublisher: ApplicationEventPublisher
 ) {
+    val log = KotlinLogging.logger {}
+
     fun publishEvent(event: CancelScheduledEvent) {
         applicationEventPublisher.publishEvent(event)
     }
@@ -30,5 +39,19 @@ class EmailEventPublisher(
 
     fun publishEvent(event: EmailSentEvent) {
         applicationEventPublisher.publishEvent(event)
+    }
+
+    suspend fun publishEvent(events: List<PostEmailTemplateEvent>) {
+        events.parMap { event ->
+            TransactionSynchronizationManager.forCurrentTransaction().map { manager ->
+                manager.registerSynchronization(object : TransactionSynchronization {
+                    override fun afterCompletion(status: Int): Mono<Void> {
+                        applicationEventPublisher.publishEvent(event)
+                        log.info { "published event: $event" }
+                        return super.afterCompletion(status)
+                    }
+                })
+            }.awaitSingleOrNull()
+        }
     }
 }
