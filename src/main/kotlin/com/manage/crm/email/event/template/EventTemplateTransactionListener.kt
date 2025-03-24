93b14549adc2fd96a17605708327dd949b780940
belljun3395 <195850@jnu.ac.kr>
2025-03-24 21:59:48 +0900

    refactor(email): 이벤트 리스너 선언 변경

--- src/main/kotlin/com/manage/crm/email/event/template/EventTemplateTransactionListener.kt
+++ src/main/kotlin/com/manage/crm/email/event/template/EventTemplateTransactionListener.kt
@@ -6,10 +6,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.transaction.event.TransactionPhase
-import org.springframework.transaction.event.TransactionalEventListener
 import org.springframework.transaction.reactive.executeAndAwait
 
 @Component
@@ -19,8 +18,13 @@ class EventTemplateTransactionListener(
 ) {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
+    /**
+     * `@EventListener`를 사용한 이유:
+     * `EmailEventPublisher`에서 트랜잭션에 `TransactionSynchronization`를 등록하는 방식으로 이벤트 발행 시점을 제어하고 있다.
+     * @see com.manage.crm.email.support.EmailEventPublisher.publishEvent
+     */
     @Async
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
+    @EventListener
     fun handleAfterCompletionEvent(event: EmailTemplateTransactionAfterCompletionEvent) {
         scope.launch {
             transactionalTemplates.newTxWriter.executeAndAwait {
