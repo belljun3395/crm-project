7cddecfe28a0e9257ead190b9918ef1176d2029e
belljun3395 <195850@jnu.ac.kr>
2025-03-24 15:34:03 +0900

    [Feat/#15] 변수에 값을 지정해서 공지 메일을 보낼 수 있도록 구현 (#22)
    * feat(user): Json 필요 메서드 추가
    
    - isExist: key를 통해 value에 key가 포함되어 있는지 확인하는 메서드
    - getValue: key를 통해 해당하는 value를 반환하는 메서드
    
    * refactor(email): MailService로 네이밍 변경
    
    - 기존의 NonVariable은 변수가 없는 경우에 한정하기 때문
    
    * feat(email): variables이 존재하지 않는지 확인하는 메서드 구현
    
    * feat(email): Variables 필요 메서드 추가
    
    - getKeyType: value의 key
    - getAttributeKey: value가 attribute인 경우 key
    - getCustomAttributeKey: value가 custom_attribute인 경우 key
    - checkValueContainType: value의 값 검증
    - findVariable: key를 통해 variable을 획득하는 메서드
    - findVariableDefault: key를 통해 variable의 기본값을 획득하는 메서드
    
    * refactor(email): SendNotificationEmailUseCase 수정 반영
    
    - getEmailNotificationProperties: variables 반환 추가
    - findAllExistByUserAttributesKey: sendType 파라미터 활용하도록 수정
    - doAssociateVariables: attribute에서 attribute와 custom_attribute 타입의 값 추출 혹은 기본 값 반환
    
    * refactor(email): NotificationEmailSendTimeOutInvokeEventHandler 수정 반영
    
    * refactor(email): EmailSendEventListenerTest 수정 반영
    
    * refactor(email): MailEventInvokeSituationTest 수정 반영
    
    * chore(email): 테스트 네이밍 명확하도록 수정
    
    - nonVariablesMailServiceImpl -> mailServiceImpl
    
    * feat(email): VariablesSupport를 사용하여 공통 로직 수정
    
    * refactor(email): user의 attributes 조회시 null 관련 처리 추가
    
    * refactor(email): when을 활용하여 분기 로직 수정

--- src/main/kotlin/com/manage/crm/user/domain/vo/Json.kt
+++ src/main/kotlin/com/manage/crm/user/domain/vo/Json.kt
@@ -5,7 +5,39 @@ import com.fasterxml.jackson.databind.ObjectMapper
 data class Json(
     val value: String
 ) {
+    fun isExist(key: String, objectMapper: ObjectMapper): Boolean {
+        objectMapper.readValue(value, Map::class.java)[key]?.let {
+            return true
+        } ?: return false
+    }
+
+    fun isExist(keys: List<String>, objectMapper: ObjectMapper): Boolean {
+        var map = objectMapper.readValue(value, Map::class.java)
+        val lastKey = keys.last()
+        for (i in 0 until keys.size - 1) {
+            map[keys[i]]?.let {
+                map = objectMapper.readValue(it.toString(), Map::class.java)
+            } ?: throw IllegalArgumentException("Key not found. key: ${keys[i]}")
+        }
+        return map.containsKey(lastKey)
+    }
+
     fun getValue(key: RequiredUserAttributeKey, objectMapper: ObjectMapper): String {
         return objectMapper.readValue(value, Map::class.java)[key.value] as String
+    }
+
+    fun getValue(key: String, objectMapper: ObjectMapper): String {
+        return objectMapper.readValue(value, Map::class.java)[key].toString()
+    }
+
+    fun getValue(keys: List<String>, objectMapper: ObjectMapper): String {
+        var map = objectMapper.readValue(value, Map::class.java)
+        val lastKey = keys.last()
+        for (i in 0 until keys.size - 1) {
+            map[keys[i]]?.let {
+                map = objectMapper.readValue(it.toString(), Map::class.java)
+            } ?: throw IllegalArgumentException("Key not found. key: ${keys[i]}")
+        }
+        return map[lastKey].toString()
     }
 }
--- src/test/kotlin/com/manage/crm/email/domain/vo/VariablesTest.kt
+++ src/test/kotlin/com/manage/crm/email/domain/vo/VariablesTest.kt
@@ -18,48 +18,72 @@ class VariablesTest : FeatureSpec({
     feature("Variables#getVariables") {
         scenario("get variables") {
             // given
-            val variables = Variables("title:hello", "name")
+            val variables = Variables("attribute_title:hello", "attribute_name")
 
             // when
             val result = variables.getVariables()
 
             // then
-            result shouldBe listOf("title:hello", "name")
+            result shouldBe listOf("attribute_title:hello", "attribute_name")
         }
 
         scenario("get variables without default") {
             // given
-            val variables = Variables("title:hello", "name")
+            val variables = Variables("attribute_title:hello", "attribute_name")
 
             // when
             val result = variables.getVariables(withDefault = false)
 
             // then
-            result shouldBe listOf("title", "name")
+            result shouldBe listOf("attribute_title", "attribute_name")
         }
     }
 
     feature("Variables#findVariable") {
         scenario("get variable") {
             // given
-            val variables = Variables("title:hello", "name")
+            val variables = Variables("attribute_title:hello", "attribute_name")
 
             // when
-            val result = variables.findVariable("title")
+            val result = variables.findVariable("attribute_title")
 
             // then
-            result shouldBe "title:hello"
+            result shouldBe "attribute_title:hello"
         }
 
         scenario("get variable without default") {
             // given
-            val variables = Variables("title:hello", "name")
+            val variables = Variables("attribute_title:hello", "attribute_name")
 
             // when
-            val result = variables.findVariable("title", withDefault = false)
+            val result = variables.findVariable("attribute_title", withDefault = false)
 
             // then
-            result shouldBe "title"
+            result shouldBe "attribute_title"
+        }
+    }
+
+    feature("Variables#findVariableDefault") {
+        scenario("get variable default") {
+            // given
+            val variables = Variables("attribute_title:hello", "attribute_name")
+
+            // when
+            val result = variables.findVariableDefault("attribute_title")
+
+            // then
+            result shouldBe "hello"
+        }
+
+        scenario("get variable default which does not have default value") {
+            // given
+            val variables = Variables("attribute_title:hello", "attribute_name")
+
+            // when
+            val result = variables.findVariableDefault("attribute_name")
+
+            // then
+            result shouldBe null
         }
     }
 })
