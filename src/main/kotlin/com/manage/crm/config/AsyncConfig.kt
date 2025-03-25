a314d447595b595d21ee6d803f6ccc80bc3883a3
belljun3395 <195850@jnu.ac.kr>
2025-03-25 13:42:05 +0900

    feat: 기본 async Executor 설정

--- src/main/kotlin/com/manage/crm/config/AsyncConfig.kt
+++ src/main/kotlin/com/manage/crm/config/AsyncConfig.kt
@@ -1,8 +1,20 @@
 package com.manage.crm.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
 
 @Configuration
 @EnableAsync
-class AsyncConfig
+class AsyncConfig : AsyncConfigurer {
+    override fun getAsyncExecutor(): Executor {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 10
+            maxPoolSize = 20
+            setThreadNamePrefix("default-async")
+            initialize()
+        }
+    }
+}
