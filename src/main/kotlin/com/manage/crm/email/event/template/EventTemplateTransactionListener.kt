18083c99793ca0b2e0dadd83d75ba2dc6913dad0
belljun3395 <195850@jnu.ac.kr>
2025-03-25 21:50:44 +0900

    [Refactor/#2] 이벤트 리스너에 적용한 코루틴 올바른 적용인지 확인 및 수정 (#39)
    * feat(email): EmailCoroutineScope 구현
    
    - eventListenerScope: 이벤트 리스너를 위한 스코프 생성 메서드
    
    * refactor(email): EmailCoroutineScope 적용
    
    - EmailCoroutineScope.eventListenerScope 메서드를 활용해 스코프 생성
    - @Async 어노테이션 삭제
    
    * refactor: AsyncConfig 삭제
    
    - 이벤트 리스너에서도 @Async가 필요하지 않음
    - 추후 비동기로 처리할 것은 코루틴을 활용하는 방법으로 처리

--- src/main/kotlin/com/manage/crm/email/event/template/EventTemplateTransactionListener.kt
+++ src/main/kotlin/com/manage/crm/email/event/template/EventTemplateTransactionListener.kt
@@ -1,13 +1,10 @@
 package com.manage.crm.email.event.template
 
 import com.manage.crm.email.event.template.handler.PostEmailTemplateEventHandler
+import com.manage.crm.email.support.EmailCoroutineScope.eventListenerScope
 import com.manage.crm.support.transactional.TransactionTemplates
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import org.springframework.context.event.EventListener
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.reactive.executeAndAwait
 
@@ -16,17 +13,14 @@ class EventTemplateTransactionListener(
     private val postEmailTemplateEventHandler: PostEmailTemplateEventHandler,
     private val transactionalTemplates: TransactionTemplates
 ) {
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-
     /**
      * `@EventListener`를 사용한 이유:
      * `EmailEventPublisher`에서 트랜잭션에 `TransactionSynchronization`를 등록하는 방식으로 이벤트 발행 시점을 제어하고 있다.
      * @see com.manage.crm.email.support.EmailEventPublisher.publishEvent
      */
-    @Async
     @EventListener
     fun handleAfterCompletionEvent(event: EmailTemplateTransactionAfterCompletionEvent) {
-        scope.launch {
+        eventListenerScope().launch {
             transactionalTemplates.newTxWriter.executeAndAwait {
                 when (event) {
                     is PostEmailTemplateEvent -> postEmailTemplateEventHandler.handle(event)
