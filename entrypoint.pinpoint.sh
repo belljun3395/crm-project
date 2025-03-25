05f60076eff327bb15539ed3e6ae1fc3f5eb63d5
belljun3395 <195850@jnu.ac.kr>
2025-03-25 11:04:00 +0900

    feat: pinpoint를 적용한 Dockerfile 적용

---
+++ entrypoint.pinpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "[INFO] Starting CRM App with Pinpoint Agent..."
+echo "PROFILE: $PINPOINT_PROFILE"
+echo "AGENT PATH: $PINPOINT_AGENT_PATH"
+
+java \
+  -XX:MaxGCPauseMillis=100 \
+  -XX:InitialRAMPercentage=50.0 \
+  -XX:MaxRAMPercentage=80.0 \
+  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 \
+  -jar \
+  -javaagent:${PINPOINT_AGENT_PATH}/pinpoint-bootstrap.jar \
+  -Dpinpoint.profiler.profiles.active=release \
+  -Dpinpoint.applicationName=crm-${PINPOINT_PROFILE} \
+  app.jar
