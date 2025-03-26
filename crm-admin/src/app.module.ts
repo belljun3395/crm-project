af4e38e3d671ac83a393034ee0fff923e38f6381
belljun3395 <195850@jnu.ac.kr>
2025-03-26 13:18:33 +0900

    feat(admin): initial admin commit

---
+++ crm-admin/src/app.module.ts
@@ -0,0 +1,72 @@
+import { Module } from '@nestjs/common';
+import * as AdminJSTypeorm from '@adminjs/typeorm';
+import AdminJS from 'adminjs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+
+AdminJS.registerAdapter({
+  Resource: AdminJSTypeorm.Resource,
+  Database: AdminJSTypeorm.Database,
+});
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
+
+@Module({
+  imports: [
+    // Use .env file for configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // Connect to the database
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT'), 10),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+    // AdminJS version 7 is ESM-only. In order to import it, you have to use dynamic imports.
+    import('@adminjs/nestjs').then(({ AdminModule }) =>
+      AdminModule.createAdminAsync({
+        useFactory: () => ({
+          adminJsOptions: {
+            rootPath: '/admin',
+            resources: [],
+          },
+          auth: {
+            authenticate,
+            cookieName: 'adminjs',
+            cookiePassword: 'secret',
+          },
+          sessionOptions: {
+            resave: true,
+            saveUninitialized: true,
+            secret: 'secret',
+          },
+        }),
+      }),
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
