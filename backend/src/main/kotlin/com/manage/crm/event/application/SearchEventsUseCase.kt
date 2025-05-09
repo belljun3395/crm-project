d045069f1fd84f767dcde5be5b82d4e98c56a216
belljun3395 <195850@jnu.ac.kr>
2025-05-09 16:43:58 +0900

    [Feat/#53] SearchEventsUseCase의 이벤트 반환시 생성 요청에서의 external_id를 동일하게 반환하도록 구현 (#71)
    * feat(event): SearchEventsUseCase의 이벤트 반환시 생성 요청에서의 external_id를 동일하게 반환하도록 구현
    
    * refactor(event): null 처리 개선

--- backend/src/main/kotlin/com/manage/crm/event/application/SearchEventsUseCase.kt
+++ backend/src/main/kotlin/com/manage/crm/event/application/SearchEventsUseCase.kt
@@ -12,16 +12,21 @@ import com.manage.crm.event.domain.repository.query.SearchByPropertyQuery
 import com.manage.crm.event.domain.vo.Properties
 import com.manage.crm.event.domain.vo.Property
 import com.manage.crm.support.out
+import com.manage.crm.user.domain.repository.UserRepository
 import org.springframework.stereotype.Service
 
 /**
  *  - `events`: `eventName`과 `properties`를 기준으로 `Event`를 검색한 결과
  *      - `eventName`은 필수 검색 조건
  *      - `properties`와 `propertyOperations`를 조홥하여 여러 조건의 검색 가능
+ *  - `users`: `events`에 포함된 `userId`를 기준으로 `User`를 검색한 결과
+ *  - `eventDto`: `events`와 `users`를 조합하여 생성한 DTO
+ *      - `externalId`는 `User`가 삭제 된 경우 조회 하지 못할 수 있다. 해당 경우 `null`로 처리한다.
  */
 @Service
 class SearchEventsUseCase(
-    private val eventRepository: EventRepository
+    private val eventRepository: EventRepository,
+    private val userRepository: UserRepository
 ) {
     suspend fun execute(useCaseIn: SearchEventsUseCaseIn): SearchEventsUseCaseOut {
         val eventName = useCaseIn.eventName
@@ -34,11 +39,15 @@ class SearchEventsUseCase(
 
         val events = searchEvents(eventName, propertyOperations)
 
+        val userIds = events.mapNotNull { it.userId }.toSet().toList()
+        val users = userRepository.findAllByIdIn(userIds).associateBy { it.id }
+
         return out {
             events.map { it ->
                 EventDto(
                     it.id!!,
                     it.name!!,
+                    it.userId?.let { users[it]?.externalId },
                     it.properties!!.value.map { SearchEventPropertyDto(it.key, it.value) }.toList(),
                     it.createdAt!!
                 )
