3f81775f66ae10ff984ca2fa3e5319128bbe4983
belljun3395 <195850@jnu.ac.kr>
2025-05-09 13:28:28 +0900

    chore: UC & Service 주석 추가 및 네이밍 개선
    -  스케줄러 서비스 인터페이스를 ScheduleTaskAllService, ScheduleTaskCommandService, ScheduleTaskQueryService로 '전체, 명령, 조회'의 의미가 더 잘 들어날 수 있도록 수정

--- backend/src/main/kotlin/com/manage/crm/event/application/PostCampaignUseCase.kt
+++ backend/src/main/kotlin/com/manage/crm/event/application/PostCampaignUseCase.kt
@@ -11,6 +11,9 @@ import com.manage.crm.support.out
 import org.springframework.stereotype.Service
 
 // TODO: check concurrency issues: existsCampaignsByName and save
+/**
+ *  - `savedCampaign`: 새로 저장한 캠페인
+ */
 @Service
 class PostCampaignUseCase(
     private val campaignRepository: CampaignRepository
