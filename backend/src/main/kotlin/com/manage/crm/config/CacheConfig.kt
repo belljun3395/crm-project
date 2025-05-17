fbe48149e1d3f71b0be366b1a5f8355c3483e86c
belljun3395 <195850@jnu.ac.kr>
2025-05-17 11:23:14 +0900

    feat: Cache 설정 추가

---
+++ backend/src/main/kotlin/com/manage/crm/config/CacheConfig.kt
@@ -0,0 +1,42 @@
+package com.manage.crm.config
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.interceptor.KeyGenerator
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.RedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableCaching
+class CacheConfig {
+    @Bean
+    fun allKeyGenerator(): KeyGenerator {
+        return KeyGenerator { _, _, _ -> "all" }
+    }
+
+    // ----------------- Redis -----------------
+    @Bean
+    fun cacheManager(@Qualifier("redisConnectionFactory") connectionFactory: RedisConnectionFactory, serializer: RedisSerializer<Any>): CacheManager {
+        return RedisCacheManager.builder(connectionFactory)
+            .cacheDefaults(redisCacheConfiguration(serializer))
+            .build()
+    }
+
+    fun redisCacheConfiguration(serializer: RedisSerializer<Any>): RedisCacheConfiguration {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .disableCachingNullValues()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+            )
+    }
+}
