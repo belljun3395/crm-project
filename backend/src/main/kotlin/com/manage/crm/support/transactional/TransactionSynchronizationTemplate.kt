79d69c2d80066654c307dffda582a63b3c17c104
belljun3395 <195850@jnu.ac.kr>
2025-05-17 22:38:53 +0900

    feat: TransactionSynchronizationTemplate 구현

---
+++ backend/src/main/kotlin/com/manage/crm/support/transactional/TransactionSynchronizationTemplate.kt
@@ -0,0 +1,34 @@
+package com.manage.crm.support.transactional
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.mono
+import org.springframework.stereotype.Service
+import org.springframework.transaction.reactive.TransactionSynchronization
+import org.springframework.transaction.reactive.TransactionSynchronizationManager
+import reactor.core.publisher.Mono
+import kotlin.coroutines.CoroutineContext
+
+@Service
+class TransactionSynchronizationTemplate {
+    val log = KotlinLogging.logger {}
+
+    suspend fun afterCompletion(
+        context: CoroutineContext = Dispatchers.IO,
+        blockDescription: String,
+        block: suspend () -> Unit
+    ) {
+        TransactionSynchronizationManager.forCurrentTransaction().map { manager ->
+            manager.registerSynchronization(object : TransactionSynchronization {
+                override fun afterCompletion(status: Int): Mono<Void> {
+                    return mono(context) {
+                        log.debug { "do after completion: $blockDescription" }
+                        block()
+                        return@mono null
+                    }
+                }
+            })
+        }.awaitSingle()
+    }
+}
