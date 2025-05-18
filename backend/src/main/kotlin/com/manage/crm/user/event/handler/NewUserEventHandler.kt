70291f3da0637b9d2764f831621c31824bcfdbf5
belljun3395 <195850@jnu.ac.kr>
2025-05-18 22:51:54 +0900

    feat(user): NewUserEventHandler 추가

---
+++ backend/src/main/kotlin/com/manage/crm/user/event/handler/NewUserEventHandler.kt
@@ -0,0 +1,26 @@
+package com.manage.crm.user.event.handler
+
+import com.manage.crm.support.coroutine.mdcCoroutineScope
+import com.manage.crm.user.domain.cache.UserCacheManager
+import com.manage.crm.user.event.NewUserEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.launch
+import org.springframework.stereotype.Component
+
+@Component
+class NewUserEventHandler(
+    private val userCacheManager: UserCacheManager
+) {
+    private val log = KotlinLogging.logger {}
+
+    /**
+     * - Increment Total User Count
+     */
+    suspend fun handle(event: NewUserEvent) {
+        mdcCoroutineScope().launch {
+            userCacheManager.incrTotalUserCount().let {
+                log.debug { "total user count: $it" }
+            }
+        }
+    }
+}
