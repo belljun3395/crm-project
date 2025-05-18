ebe2cd120fb3be8c4954ffc8cebb1c1ac47c3407
belljun3395 <195850@jnu.ac.kr>
2025-05-18 22:48:23 +0900

    feat(user): UserEventPublisher 구현

---
+++ backend/src/main/kotlin/com/manage/crm/user/support/UserEventPublisher.kt
@@ -0,0 +1,26 @@
+package com.manage.crm.user.support
+
+import com.manage.crm.support.transactional.TransactionSynchronizationTemplate
+import com.manage.crm.user.event.NewUserEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.slf4j.MDCContext
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Service
+
+@Service
+class UserEventPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher,
+    private val transactionSynchronizationTemplate: TransactionSynchronizationTemplate
+) {
+    val log = KotlinLogging.logger {}
+
+    suspend fun publish(event: NewUserEvent) {
+        transactionSynchronizationTemplate.afterCompletion(
+            Dispatchers.Default + MDCContext(),
+            "publish event: $event"
+        ) {
+            applicationEventPublisher.publishEvent(event)
+        }
+    }
+}
