7fc397742c7718675080bdc14118198d5d123ace
belljun3395 <195850@jnu.ac.kr>
2025-05-18 22:52:11 +0900

    feat(user): UserTransactionEventListener 추가

---
+++ backend/src/main/kotlin/com/manage/crm/user/event/UserTransactionEventListener.kt
@@ -0,0 +1,31 @@
+package com.manage.crm.user.event
+
+import com.manage.crm.support.coroutine.eventListenerCoroutineScope
+import com.manage.crm.support.transactional.TransactionTemplates
+import com.manage.crm.user.event.handler.NewUserEventHandler
+import kotlinx.coroutines.launch
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import org.springframework.transaction.reactive.executeAndAwait
+
+@Component
+class UserTransactionEventListener(
+    private val newUserEventHandler: NewUserEventHandler,
+    private val transactionalTemplates: TransactionTemplates
+) {
+    /**
+     * `@EventListener`를 사용한 이유:
+     * `EmailEventPublisher`에서 트랜잭션에 `TransactionSynchronization`를 등록하는 방식으로 이벤트 발행 시점을 제어하고 있다.
+     * @see com.manage.crm.user.support.UserEventPublisher
+     */
+    @EventListener
+    fun handleAfterCompletionEvent(event: UserTransactionAfterCompletionEvent) {
+        eventListenerCoroutineScope().launch {
+            transactionalTemplates.newTxWriter.executeAndAwait {
+                when (event) {
+                    is NewUserEvent -> newUserEventHandler.handle(event)
+                }
+            }
+        }
+    }
+}
