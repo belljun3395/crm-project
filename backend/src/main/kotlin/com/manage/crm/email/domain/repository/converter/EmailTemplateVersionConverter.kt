7f64bf0e95e1c0bef13e0281b7daa4ea43b4510e
belljun3395 <195850@jnu.ac.kr>
2025-05-19 14:07:30 +0900

    [Fix/#91] ROW로 R2DBC 컨버터 구체적 타입으로 수정 (#92)
    * fix(email): ROW로 R2DBC 컨버터 구체적 타입으로 수정
    
    * fix(email): 등록되어 있지 않았던 컨버터 등록
    
    - UserEmailConverter

--- backend/src/main/kotlin/com/manage/crm/email/domain/repository/converter/EmailTemplateVersionConverter.kt
+++ backend/src/main/kotlin/com/manage/crm/email/domain/repository/converter/EmailTemplateVersionConverter.kt
@@ -1,16 +1,14 @@
 package com.manage.crm.email.domain.repository.converter
 
 import com.manage.crm.email.domain.vo.EmailTemplateVersion
-import io.r2dbc.spi.Row
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.ReadingConverter
 import org.springframework.data.convert.WritingConverter
 
 @ReadingConverter
-class EmailTemplateVersionReadingConverter : Converter<Row, EmailTemplateVersion> {
-    override fun convert(source: Row): EmailTemplateVersion? {
-        val value = source.get("version", Float::class.java)
-        return value?.let { EmailTemplateVersion(it) }
+class EmailTemplateVersionReadingConverter : Converter<Float, EmailTemplateVersion> {
+    override fun convert(source: Float): EmailTemplateVersion {
+        return EmailTemplateVersion(source)
     }
 }
 
