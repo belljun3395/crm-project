b34af635b1d27fe7995126f2eb5486a3d8aaa416
belljun3395 <195850@jnu.ac.kr>
2025-05-19 13:24:05 +0900

    refactor(email): 핸들러에 적용된 transactionalTemplates 적용 해제

--- backend/src/main/kotlin/com/manage/crm/email/event/send/handler/EmailOpenEventHandler.kt
+++ backend/src/main/kotlin/com/manage/crm/email/event/send/handler/EmailOpenEventHandler.kt
@@ -3,15 +3,12 @@ package com.manage.crm.email.event.send.handler
 import com.manage.crm.email.domain.repository.EmailSendHistoryRepository
 import com.manage.crm.email.domain.vo.SentEmailStatus
 import com.manage.crm.email.event.send.EmailOpenEvent
-import com.manage.crm.support.transactional.TransactionTemplates
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Component
-import org.springframework.transaction.reactive.executeAndAwait
 
 @Component
 class EmailOpenEventHandler(
-    private val emailSendHistoryRepository: EmailSendHistoryRepository,
-    private val transactionalTemplates: TransactionTemplates
+    private val emailSendHistoryRepository: EmailSendHistoryRepository
 ) {
     val log = KotlinLogging.logger {}
 
@@ -19,14 +16,12 @@ class EmailOpenEventHandler(
      * - Update the status of the email to "OPEN"
      */
     suspend fun handle(event: EmailOpenEvent) {
-        transactionalTemplates.writer.executeAndAwait {
-            emailSendHistoryRepository
-                .findByEmailMessageId(event.messageId)
-                ?.let {
-                    it.sendStatus = SentEmailStatus.OPEN.name
-                    emailSendHistoryRepository.save(it)
-                }
-                ?: log.error { "EmailSendHistory not found by email messageId: ${event.messageId}" }
-        }
+        emailSendHistoryRepository
+            .findByEmailMessageId(event.messageId)
+            ?.let {
+                it.sendStatus = SentEmailStatus.OPEN.name
+                emailSendHistoryRepository.save(it)
+            }
+            ?: log.error { "EmailSendHistory not found by email messageId: ${event.messageId}" }
     }
 }
--- backend/src/main/kotlin/com/manage/crm/email/event/template/handler/PostEmailTemplateEventHandler.kt
+++ backend/src/main/kotlin/com/manage/crm/email/event/template/handler/PostEmailTemplateEventHandler.kt
@@ -4,40 +4,31 @@ import com.manage.crm.email.domain.EmailTemplateHistory
 import com.manage.crm.email.domain.repository.EmailTemplateHistoryRepository
 import com.manage.crm.email.domain.repository.EmailTemplateRepository
 import com.manage.crm.email.event.template.PostEmailTemplateEvent
-import com.manage.crm.support.coroutine.mdcCoroutineScope
-import com.manage.crm.support.transactional.TransactionTemplates
-import kotlinx.coroutines.launch
 import org.springframework.stereotype.Component
-import org.springframework.transaction.reactive.executeAndAwait
 
 @Component
 class PostEmailTemplateEventHandler(
     private val emailTemplateRepository: EmailTemplateRepository,
-    private val emailTemplateHistoryRepository: EmailTemplateHistoryRepository,
-    private val transactionalTemplates: TransactionTemplates
+    private val emailTemplateHistoryRepository: EmailTemplateHistoryRepository
 ) {
     /**
      * - Save Email Template History
      */
     suspend fun handle(event: PostEmailTemplateEvent) {
-        mdcCoroutineScope().launch {
-            transactionalTemplates.writer.executeAndAwait {
-                val templateId = event.templateId
-                val template =
-                    emailTemplateRepository
-                        .findById(templateId)
-                        ?: throw IllegalArgumentException("EmailTemplate not found by id: $templateId")
+        val templateId = event.templateId
+        val template =
+            emailTemplateRepository
+                .findById(templateId)
+                ?: throw IllegalArgumentException("EmailTemplate not found by id: $templateId")
 
-                emailTemplateHistoryRepository.save(
-                    EmailTemplateHistory.new(
-                        templateId = template.id!!,
-                        subject = template.subject,
-                        body = template.body,
-                        variables = template.variables,
-                        version = template.version
-                    )
-                )
-            }
-        }
+        emailTemplateHistoryRepository.save(
+            EmailTemplateHistory.new(
+                templateId = template.id!!,
+                subject = template.subject,
+                body = template.body,
+                variables = template.variables,
+                version = template.version
+            )
+        )
     }
 }
