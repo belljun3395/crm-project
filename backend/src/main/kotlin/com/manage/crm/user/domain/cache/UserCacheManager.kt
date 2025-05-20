49e1b1f11cceea7eb83f4b542fc537bef07cd8ee
belljun3395 <195850@jnu.ac.kr>
2025-05-20 17:59:36 +0900

    feat(user): 유저 캐시 메서드 추가 구현
    - 총 유저 수 뿐만 아니라 등록 시간 정보도 함께 저장
    
    - totalUserCount: 총 유저 구하기
    - saveTotalUserCount: 총 유저 저장

--- backend/src/main/kotlin/com/manage/crm/user/domain/cache/UserCacheManager.kt
+++ backend/src/main/kotlin/com/manage/crm/user/domain/cache/UserCacheManager.kt
@@ -1,15 +1,36 @@
 package com.manage.crm.user.domain.cache
 
+import com.manage.crm.user.event.RefreshTotalUsersCommand
+import com.manage.crm.user.support.UserEventPublisher
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class UserCacheManager(
-    private val redisTemplate: ReactiveRedisTemplate<String, Any>
+    private val redisTemplate: ReactiveRedisTemplate<String, Any>,
+    private val userEventPublisher: UserEventPublisher
 ) {
     companion object {
-        const val TOTAL_USER_COUNT_KEY = "user::total"
+        private const val SPLIT = "::"
+        private const val USER_CACHE_KEY = "user"
+        const val USER_CACHE_KEY_PREFIX = USER_CACHE_KEY + SPLIT
+        const val TOTAL_USER_COUNT_KEY = USER_CACHE_KEY_PREFIX + "total"
+        const val TOTAL_USER_COUNT_UPDATED_AT_KEY = TOTAL_USER_COUNT_KEY + SPLIT + "updatedAt"
+        private const val REFRESH_INTERVAL_MS = 1000L * 60 * 60 * 3 // 3 hours
+    }
+
+    suspend fun totalUserCount(): Long {
+        val count = redisTemplate.opsForValue()
+            .get(TOTAL_USER_COUNT_KEY)
+            .awaitSingle() as Long? ?: 0L
+        val updatedAt = redisTemplate.opsForValue()
+            .get(TOTAL_USER_COUNT_UPDATED_AT_KEY)
+            .awaitSingle() as Long? ?: 0L
+        if (updatedAt < System.currentTimeMillis() - REFRESH_INTERVAL_MS) {
+            userEventPublisher.publish(RefreshTotalUsersCommand(count))
+        }
+        return count
     }
 
     suspend fun incrTotalUserCount(): Long {
@@ -17,8 +38,26 @@ class UserCacheManager(
     }
 
     suspend fun incr(key: String): Long {
-        return redisTemplate.opsForValue()
+        val newCount = redisTemplate.opsForValue()
             .increment(key)
             .awaitSingle()
+        redisTemplate.opsForValue()
+            .set(TOTAL_USER_COUNT_UPDATED_AT_KEY, System.currentTimeMillis())
+            .awaitSingle()
+        return newCount
+    }
+
+    suspend fun saveTotalUserCount(count: Long): Long {
+        val isSaved = redisTemplate.opsForValue()
+            .set(TOTAL_USER_COUNT_KEY, count)
+            .awaitSingle()
+        redisTemplate.opsForValue()
+            .set(TOTAL_USER_COUNT_UPDATED_AT_KEY, System.currentTimeMillis())
+            .awaitSingle()
+        if (isSaved) {
+            return count
+        } else {
+            throw IllegalStateException("Failed to save total user count")
+        }
     }
 }
