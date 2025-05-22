f6666ab68f84fd1a214f56224915a743855662af
belljun3395 <195850@jnu.ac.kr>
2025-05-22 17:21:04 +0900

    Revert "[Feat/#96] 요청/응답 압축 구현 (#97)" (#98)
    This reverts commit 82a51165154ba6f850c51d11ce8eee4f8ee0fb54.

--- backend/src/main/kotlin/com/manage/crm/config/web/compression/CompressionException.kt
+++
@@ -1,7 +0,0 @@
-package com.manage.crm.config.web.compression
-
-class IllegalCompressionRequestException(message: String) : RuntimeException(message)
-
-class IllegalCompressionResponseException(message: String) : RuntimeException(message)
-
-class CompressionException
