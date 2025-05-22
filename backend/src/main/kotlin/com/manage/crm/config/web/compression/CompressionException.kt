82a51165154ba6f850c51d11ce8eee4f8ee0fb54
belljun3395 <195850@jnu.ac.kr>
2025-05-22 13:03:07 +0900

    [Feat/#96] 요청/응답 압축 구현 (#97)
    * feat: commons-io 의존성 추가
    
    * feat: 압축 설정 클래스 추가
    
    * feat: Gzip 압축 클래스 추가
    
    * feat: 압축 구성 추가
    
    * refactor: 리뷰 사항 반영
    
    * chore: 공통 린트 적용
    
    * refactor: 로그 메시지 구체화
    
    * refactor: 미사용 메서드 삭제

---
+++ backend/src/main/kotlin/com/manage/crm/config/web/compression/CompressionException.kt
@@ -0,0 +1,7 @@
+package com.manage.crm.config.web.compression
+
+class IllegalCompressionRequestException(message: String) : RuntimeException(message)
+
+class IllegalCompressionResponseException(message: String) : RuntimeException(message)
+
+class CompressionException
