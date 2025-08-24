7418ca720497afc379ff463c4ac151eedd9a8eac
belljun3395 <195850@jnu.ac.kr>
2025-08-25 06:24:08 +0900

    refactor: VariablesSupport to simplify key type checks and enhance attribute retrieval

--- backend/src/main/kotlin/com/manage/crm/email/domain/support/VariablesSupport.kt
+++ backend/src/main/kotlin/com/manage/crm/email/domain/support/VariablesSupport.kt
@@ -4,27 +4,27 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.manage.crm.email.domain.vo.ATTRIBUTE_TYPE
 import com.manage.crm.email.domain.vo.CUSTOM_ATTRIBUTE_TYPE
 import com.manage.crm.email.domain.vo.Variables
-import com.manage.crm.email.domain.vo.getAttributeKey
-import com.manage.crm.email.domain.vo.getCustomAttributeKey
-import com.manage.crm.email.domain.vo.getKeyType
+import com.manage.crm.email.domain.vo.getType
 import com.manage.crm.user.domain.vo.Json
 
 class VariablesSupport {
     companion object {
         fun doAssociate(objectMapper: ObjectMapper, key: String, attributes: Json, variables: Variables): Pair<String, String> {
             return when {
-                key.getKeyType() == ATTRIBUTE_TYPE && attributes.isExist(key.getAttributeKey(), objectMapper) -> {
-                    key to attributes.getValue(key.getAttributeKey(), objectMapper)
-                }
-
-                key.getKeyType() == CUSTOM_ATTRIBUTE_TYPE && attributes.isExist(key.getCustomAttributeKey(), objectMapper) -> {
-                    key to attributes.getValue(key.getCustomAttributeKey(), objectMapper)
+                (key.getType() == ATTRIBUTE_TYPE || key.getType() == CUSTOM_ATTRIBUTE_TYPE) &&
+                    attributes.isExist(key, objectMapper) -> {
+                    key to attributes.getValue(key, objectMapper)
                 }
 
                 else -> {
                     key to (variables.findVariableDefault(key) ?: "")
                 }
             }
+        }
+
+        fun variablesAllMatchedWithKey(variables: Variables, keys: Set<String>): Boolean {
+            val variableKeys = variables.getVariables(false).toSet()
+            return variableKeys.isNotEmpty() && variableKeys.all { keys.contains(it) }
         }
     }
 }
