9be64575eff3fa1d028b053b0d088a1c756abddd
belljun3395 <195850@jnu.ac.kr>
2026-01-21 20:25:50 +0900

    [Refactor/#148] Separate event and campaign properties (#169)
    * [Refactor/#148] Separate event and campaign properties
    
    * [Fix/#148] Resolve lint warnings
    
    * [Fix/#148] Align campaign properties in tests
    
    * Fix lint: Remove unused EventProperty import

--- backend/src/main/kotlin/com/manage/crm/config/R2dbcConfig.kt
+++ backend/src/main/kotlin/com/manage/crm/config/R2dbcConfig.kt
@@ -8,8 +8,10 @@ import com.manage.crm.email.domain.repository.converter.UserEmailReadingConverte
 import com.manage.crm.email.domain.repository.converter.UserEmailWritingConverter
 import com.manage.crm.email.domain.repository.converter.VariablesReadingConverter
 import com.manage.crm.email.domain.repository.converter.VariablesWritingConverter
-import com.manage.crm.event.domain.repository.converter.PropertiesReadingConverter
-import com.manage.crm.event.domain.repository.converter.PropertiesWritingConverter
+import com.manage.crm.event.domain.repository.converter.CampaignPropertiesReadingConverter
+import com.manage.crm.event.domain.repository.converter.CampaignPropertiesWritingConverter
+import com.manage.crm.event.domain.repository.converter.EventPropertiesReadingConverter
+import com.manage.crm.event.domain.repository.converter.EventPropertiesWritingConverter
 import com.manage.crm.user.domain.repository.converter.UserAttributeReadingConverter
 import com.manage.crm.user.domain.repository.converter.UserAttributeWritingConverter
 import io.r2dbc.spi.ConnectionFactory
@@ -51,8 +53,10 @@ class R2dbcConfig {
                 VariablesReadingConverter(),
                 EventIdWritingConverter(),
                 EventIdReadingConverter(),
-                PropertiesWritingConverter(),
-                PropertiesReadingConverter(),
+                EventPropertiesWritingConverter(),
+                EventPropertiesReadingConverter(),
+                CampaignPropertiesWritingConverter(),
+                CampaignPropertiesReadingConverter(),
                 EmailTemplateVersionReadingConverter(),
                 EmailTemplateVersionWritingConverter(),
                 UserEmailReadingConverter(),
